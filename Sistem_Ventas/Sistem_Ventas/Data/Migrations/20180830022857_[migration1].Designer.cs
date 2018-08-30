b414671ea297b80a77e2236c978a476cb2742c54
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-08-29 23:41:49 -0300

    Criação da base de dados s2 a6

--- Sistem_Ventas/Sistem_Ventas/Data/Migrations/20180830022857_[migration1].Designer.cs
+++ Sistem_Ventas/Sistem_Ventas/Data/Migrations/20180830022857_[migration1].Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sistem_Ventas.Data;
 
 namespace Sistem_Ventas.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180830022857_[migration1]")]
+    partial class migration1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
