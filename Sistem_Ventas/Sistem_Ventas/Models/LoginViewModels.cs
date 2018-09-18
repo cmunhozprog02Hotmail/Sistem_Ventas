90e1888623e45054086eac1fe48d484ae3512953
Christovam Munhoz <cmunhozprog02@hotmail.com>
2018-09-17 23:55:37 -0300

    s3 a13

---
+++ Sistem_Ventas/Sistem_Ventas/Models/LoginViewModels.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sistem_Ventas.Models
+{
+    public class LoginViewModels
+    {
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+        
+    }
+}
